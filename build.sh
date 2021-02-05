8a52e2fee9e805b2f9e4de792e46f32d79408f37
Matt Curfman <matt@tecnovelty.com>
2021-02-05 11:11:09 -0800

    Initial version

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker build -t gpu_drivers_and_onevpl:rhel82 -f Dockerfile:rhel82 .
+docker build -t gpu_drivers_and_onevpl:suse15.2 -f Dockerfile:suse15.2 .
+docker build -t gpu_drivers_and_onevpl:ubuntu20.04 -f Dockerfile:ubuntu20.04 .
